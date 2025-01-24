d8e0c963c019416872aeac2b98a55d5bb9f6a2cf
Fernandofgvc <ffgvc1405@gmail.com>
2025-01-24 08:28:07 -0300

    Seller entity class

--- src/Application/App.java
+++ src/Application/App.java
@@ -1,11 +1,19 @@
 package Application;
 
+import java.time.LocalDate;
+
+
 import Model.Entities.Department;
+import Model.Entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        
+        System.out.println(seller);
     }
 }
