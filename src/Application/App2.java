d07573be27d4cc5cf3414fa12686c7ae18227827
Fernandofgvc <ffgvc1405@gmail.com>
2025-01-25 16:28:45 -0300

    insert departmentDaoJDBC implementation

--- src/Application/App2.java
+++ src/Application/App2.java
@@ -9,17 +9,21 @@ import Model.Entities.Department;
 public class App2 {
     public static void main(String[] args) throws Exception{
 
-        System.out.println("=== Test 1: Department findById ===");
+        System.out.println("=== Test 1: department findById ===");
         DepartmentDao departmentDao = DaoFactory.creatDepartmentDao();
         Department department = departmentDao.findById(6);
         System.out.println(department);
 
-        System.out.println("=== Test 2: Department findAll ===");
+        System.out.println("=== Test 2: department findAll ===");
         List<Department> list = departmentDao.findAll();
         for(Department obj : list){
             System.out.println(obj);
         }
 
-    }
+        System.out.println("\n=== Test 3: department inset =====");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);;
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+  }
 
 }
